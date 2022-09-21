0103479ec32966ff4535a0dc3c19701588d9d624
Rodrigo Laime <rodrigolaime141@gmail.com>
2022-09-20 21:43:31 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const menu = document.querySelector('.invicible');
+const barra = document.querySelector('.barra');
+const exit = document.querySelector('.exit')
+
+barra.addEventListener('click', () => {
+    menu.classList.remove('active');
+    barra.classList.add('active');
+})
+exit.addEventListener('click', () => {
+    barra.classList.remove('active');
+    menu.classList.add('active');
+})
