4a7e46fd3f82f8dd059753aa1db368154ed1b6d4
rodrigo laime <rodrigolaime141@gmail.com>
2022-09-29 11:11:56 -0300

    modifie menu

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const barra = document.querySelector('.barra');
 const exit = document.querySelector('.exit')
 
 barra.addEventListener('click', () => {
-    menu.classList.remove('active');
-    barra.classList.add('active');
+  menu.classList.remove('active');
+  barra.classList.add('active');
 })
 exit.addEventListener('click', () => {
-    barra.classList.remove('active');
-    menu.classList.add('active');
+  barra.classList.remove('active');
+  menu.classList.add('active');
 })
