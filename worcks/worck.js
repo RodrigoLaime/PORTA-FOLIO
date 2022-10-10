26c9a592b6defa29458f340a6b8f9d3a4478d4b6
Rodrigo Laime <rodrigolaime141@gmail.com>
2022-10-10 09:01:50 -0300

    modifie worck styles img

---
+++ worcks/worck.js
@@ -0,0 +1,82 @@
+const botonImagen = document.getElementById("btn-img");
+const btnImg1 = document.querySelector('.uno');
+const btnImg2 = document.querySelector('.dos');
+const btnImg3 = document.querySelector('.tres');
+const btnImg4 = document.querySelector('.cuatro');
+const btnImg5 = document.querySelector('.cinco');
+const btnImg6 = document.querySelector('.seis');
+const btnImg7 = document.querySelector('.siete');
+const btnImg8 = document.querySelector('.ocho');
+const btnImg9 = document.querySelector('.nueve');
+const btnImg10 = document.querySelector('.dies');
+
+/* botonImagen.addEventListener("click", imagenes()); */
+btnImg1.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/clon-google1.png",
+        imageHeight: 300,
+      });
+});
+btnImg2.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/batata (1).png",
+        imageHeight: 300,
+      });
+});
+btnImg3.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/faceboock.png",
+        imageHeight: 300,
+      });
+});
+btnImg4.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/tiktok.png",
+        imageHeight: 300,
+      });
+});
+btnImg5.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/cardshoping.png",
+        imageHeight: 300,
+      });
+});
+btnImg6.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/portafolio1.png",
+        imageHeight: 300,
+      });
+});
+btnImg7.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/portafolio2.png",
+        imageHeight: 300,
+      });
+});
+btnImg8.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/steps.png",
+        imageHeight: 300,
+      });
+});
+btnImg9.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/game.png",
+        imageHeight: 300,
+      });
+});
+btnImg10.addEventListener("click", () =>{
+    Swal.fire({
+        imageUrl: "../img/music.png",
+        imageHeight: 300,
+      });
+});
+
+
+/* function imagenes(){
+    Swal.fire({
+        imageUrl: "../img/clon-google1.png",
+        imageHeight: 300,
+      });
+} */
+
