8b9974d814814c497d4223ac4cc7a800ce0a9968
Rodrigo Laime <rodrigolaime141@gmail.com>
2023-01-13 12:17:13 -0300

    file contact text

---
+++ js/about.js
@@ -0,0 +1,13 @@
+const menuIcon = document.getElementById("menu-icon");
+const menuMovil = document.getElementById("menu-movil");
+const main = document.getElementById("container-main");
+
+menuIcon.addEventListener("click", () => {
+  menuMovil.classList.remove("active");
+  menuIcon.classList.add("active");
+});
+
+main.addEventListener("click", () => {
+    menuMovil.classList.add("active");
+  menuIcon.classList.remove("active");
+});
